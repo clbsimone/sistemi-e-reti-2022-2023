f6dfe7bac1c2a0291d31c653494f820af6f05d15
Simone Basso <113350437+clbsimone@users.noreply.github.com>
2022-10-07 10:59:24 +0200

    Update es18.py

---
+++ es18.py
@@ -0,0 +1,11 @@
+n1 = int(input("numero 1: "))
+n2 = int(input("numero 2: "))
+
+l = []
+
+l.append (n1 ** 2 + n2 ** 2) 
+l.append  (n1 + n2 + n1 + n2)
+l.append  (n1 ** 2 - n2 ** 2)
+l.append (n1 - n2 + n1 - n2)
+
+print(l)
