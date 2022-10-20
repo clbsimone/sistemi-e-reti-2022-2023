989103014fb75a95e14d32a063ecde6340063dcf
Simone Basso <113350437+clbsimone@users.noreply.github.com>
2022-10-20 08:19:13 +0200

    Update lista-primi.py

--- lista-primi.py
+++ lista-primi.py
@@ -1,18 +1,13 @@
 import math
 
 def isPrimo(x):
-    for i in (2, int(math.sqrt(x)) + 1):
+    for i in range(2, int(math.sqrt(x)) + 1):
         if x % i == 0:
             return False
     return True
 
 n = 100
-l = []
 
-#l = [i for i in range(1, n) if isPrimo(i)]
-
-for i in range(1, n):
-    if isPrimo(i):
-        l.append(i)
+l = [i for i in range(1, n) if isPrimo(i)]
 
 print(l)
